a7153f124c0d669b8fba22453cc5fc772fe44802
Pop Cadmi <cadmy05@yahoo.com>
2019-02-04 14:20:15 +0200

    LastWorking

--- LinqFacts/ListProductsFacts.cs
+++ LinqFacts/ListProductsFacts.cs
@@ -5,8 +5,10 @@ using Xunit;
 using Xunit.Sdk;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
+using System.Xml.Serialization;
 
 namespace LinqFacts
 {
@@ -22,46 +24,65 @@ namespace LinqFacts
         {
             public int Id { get; set; }
         }
-
-
-        [Fact]
-        public void Intially_the_list_is_empty()
-        {
-            var a = new List<Product> {};
-
-            Assert.Empty(a);
-        }
         
         [Fact]
         public void LinqTests()
         {
-            var product1 = new Product {Name = "Vasile"};
+            var product1 = new Product
+            {
+                Name = "Vasile",
+                Features = new List<Feature> { new Feature { Id = 0 } }
+            };
 
             var product2 = new Product
             {
                 Name = "Ion",
-
                 Features = new List<Feature> {new Feature {Id = 1}, new Feature {Id = 2}, new Feature {Id = 3}}
             };
 
             var product3 = new Product
             {
                 Name = "Gheroghe",
-
-                Features = new List<Feature> {new Feature {Id = 5}}
+                Features = new List<Feature> {new Feature {Id = 1}}
             };
 
             var a = new List<Product>{product1, product2, product3};
-            var b = ProductsWithAtleastOneFeature(a);
+            var aFeatures = new List<Feature> {new Feature {Id = 1}, new Feature {Id = 2}, new Feature {Id = 3}};
+
+            var b = ProductsWithAtleastOneFeature(a,aFeatures);
+            var c = ProductsWithAlltFeature(a,aFeatures);
+            var d = ProductsWithNoFeatures(a, aFeatures);
+
             Assert.Equal(new List<Product>{product2,product3}, b);
+            Assert.Equal(new List<Product> { product2}, c);
+            Assert.Equal(new List<Product> { product1 }, d);
         }
-        public List<Product> ProductsWithAtleastOneFeature(List<Product> list)
+        public List<Product> ProductsWithAtleastOneFeature(List<Product> list, List<Feature> listFeatures )
         {
-            var b = list.Select(x => x).Where(x => x.Features != null);
-            //var b = from n in list
-            //    where n.Features != null
-            //    select n;
-            return new List<Product>(b);
+            var dfs = list.Where(p => HasOneOfTheGivenFeatures(p, listFeatures));
+            return new List<Product>(dfs);
         }
+
+        public List<Product> ProductsWithAlltFeature(List<Product> list, List<Feature> listFeatures)
+        {
+            var dfs = list.Where(p => HasAllGivenFeatures(p, listFeatures));
+            return new List<Product>(dfs);
+        }
+
+        public List<Product> ProductsWithNoFeatures(List<Product> list, List<Feature> listFeatures)
+        {
+            var dfs = list.Where(p => HasAllNOFeatures(p, listFeatures));
+            return new List<Product>(dfs);
+        }
+
+        private bool HasOneOfTheGivenFeatures(Product product, List<Feature> listFeatures) 
+            => product.Features != null && product.Features.Any(f => listFeatures.Any(lf => lf.Id == f.Id));
+
+        private bool HasAllGivenFeatures(Product product, List<Feature> listFeatures)
+            => product.Features != null && listFeatures.All(f => product.Features.Any(lf => lf.Id == f.Id));
+
+        private bool HasAllNOFeatures(Product product, List<Feature> listFeatures)
+            => product.Features != null && listFeatures.All(f => product.Features.All(lf => lf.Id != f.Id));
+
     }
 }
