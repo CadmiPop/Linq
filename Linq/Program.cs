a0d498ed8c794cbf81237b4ca0079b16dbeaf662
Pop Cadmi <cadmy05@yahoo.com>
2019-02-25 17:06:19 +0200

    1

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace Linq
@@ -29,10 +30,30 @@ namespace Linq
                 new List<Photo>(files.Select(p => new Photo() {Name = p, Taken = File.GetCreationTime(p)}));
 
 
-            Rename();
 
-            void Rename()
+            Sort();
+
+
+
+            void Sort()
             {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    DirectoryInfo prefix = Directory.CreateDirectory(args[1]);
+
+                    List<string> options = new List<string>(args[2].Split('/', '-'));
+                    if (args[2].Contains("/"))
+                        prefix = Directory.CreateDirectory(args[1] + "\\" + GetOptions(list, i, options) + "\\" +
+                                                           GetOptions(list, i, options) + "\\" + GetOptions(list, i, options));
+                    if (args[2].Contains("-"))
+                        prefix = Directory.CreateDirectory(args[1] + "\\" + GetOptions(list, i, options) + "-" +
+                                                           GetOptions(list, i, options) + "-" + GetOptions(list, i, options));
+
+                    
+
+                    File.Copy(args[0] + "\\" + Path.GetFileName(list[i].Name),prefix + "\\" + Path.GetFileName(list[i].Name));
+                }
+
                 for (int i = 0; i < list.Count; i++)
                 {
                     FileInfo fi = new FileInfo(Path.GetFullPath(args[3]));
@@ -41,38 +62,27 @@ namespace Linq
                         fi.MoveTo(Path.GetDirectoryName(fi.ToString()) + "\\" + args[4] + ".jpg");
                     }
                 }
-            }
 
-            void ChangeExtentison(List<Photo> listA, string[] argsA)
+            }
+        }
+
+        private static string GetOptions(List<Photo> list, int i, List<string> options)
+        {
+            for (int j = 0; j < options.Count; j++)
             {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    var myfile = args[0] + "\\" + Path.GetFileName(list[i].Name);
-                    File.Move(myfile, Path.ChangeExtension(myfile, args[5]));
-                }
+                if (options[j] == "YY")
+                    return options[j] = options[j].Replace("YY", list[i].Taken.Year.ToString().Substring(2));
+                if (options[j] == "YYYY")
+                    return options[j] = options[j].Replace("YYYY", list[i].Taken.Year.ToString());
+                if (options[j] == "MM")
+                    return options[j] = "0" + options[j].Replace("MM", list[i].Taken.Month.ToString());
+                if (options[j] == "DD")
+                    return options[j] = options[j].Replace("DD", list[i].Taken.Day.ToString());
+                if (options[j] == "/")
+                    return "\\";
             }
 
-            void Sort(List<Photo> listA, string[] argsA)
-            {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    DirectoryInfo prefix = null;
-
-                    if (args[2] == "YYYY/MM/DD")
-                    {
-                         prefix = Directory.CreateDirectory(args[1] + "\\" + list[i].Taken.Year + "\\" + list[i].Taken.Month + "\\" +list[i].Taken.Day);
-                    }
-                    if (args[2] == "YYYY/DD/MM")
-                    {
-                        prefix = Directory.CreateDirectory(args[1] + "\\" + list[i].Taken.Year + "\\" + list[i].Taken.Day + "\\" + list[i].Taken.Month);
-                    }
-                    if (args[2] == "DD/MM/YYYY")
-                    {
-                        prefix = Directory.CreateDirectory(args[1] + "\\" + list[i].Taken.Day + "\\" + list[i].Taken.Month + "\\" + list[i].Taken.Year);
-                    }
-                    File.Copy(args[0] + "\\" + Path.GetFileName(list[i].Name),prefix + "\\" + Path.GetFileName(list[i].Name));
-                }
-            }
+            return null;
         }
     }
 }
