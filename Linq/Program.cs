e80640c7a8e09394aa9c6e580e35a232e210df27
Pop Cadmi <cadmy05@yahoo.com>
2019-02-26 14:06:11 +0200

    AZI

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
@@ -16,73 +17,43 @@ namespace Linq
         static void Main(string[] args)
         {
             var files = new string[] { };
-            if (args[0] == null)
+
+            var source = args[0].Substring(8);
+            var sourceDestionation = args[1].Substring(19);
+            var folderType = args[2].Substring(12);
+            var prefix = args[3].Substring(8);
+
+
+            if (source == null)
             {
                 Console.WriteLine("Args can't be null");
             }
 
-            if (Directory.Exists(args[0]))
+            if (Directory.Exists(source))
             {
-                files = Directory.GetFiles(args[0]);
+                files = Directory.GetFiles(source);
             }
 
             List<Photo> list =
                 new List<Photo>(files.Select(p => new Photo() {Name = p, Taken = File.GetCreationTime(p)}));
 
-
-
-            Sort();
-
-
-
-            void Sort()
+            for (int i = 0; i < list.Count; i++)
             {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    DirectoryInfo prefix = Directory.CreateDirectory(args[1]);
 
-                    List<string> options = new List<string>(args[2].Split('/', '-'));
-                    if (args[2].Contains("/"))
-                        prefix = Directory.CreateDirectory(args[1] + "\\" + GetOptions(list, i, options) + "\\" +
-                                                           GetOptions(list, i, options) + "\\" + GetOptions(list, i, options));
-                    if (args[2].Contains("-"))
-                        prefix = Directory.CreateDirectory(args[1] + "\\" + GetOptions(list, i, options) + "-" +
-                                                           GetOptions(list, i, options) + "-" + GetOptions(list, i, options));
+                string directory = Directory.CreateDirectory(sourceDestionation + "\\"
+                                                                                  + list[i].Taken
+                                                                                      .ToString(folderType,
+                                                                                          CultureInfo.InvariantCulture)
+                                                                                      .Replace("/", "\\")).FullName;
 
-                    
-
-                    File.Copy(args[0] + "\\" + Path.GetFileName(list[i].Name),prefix + "\\" + Path.GetFileName(list[i].Name));
-                }
-
-                for (int i = 0; i < list.Count; i++)
-                {
-                    FileInfo fi = new FileInfo(Path.GetFullPath(args[3]));
-                    if (fi.Exists)
-                    {
-                        fi.MoveTo(Path.GetDirectoryName(fi.ToString()) + "\\" + args[4] + ".jpg");
-                    }
-                }
-
+                FileInfo file = new FileInfo(Path.GetFullPath(list[i].Name));
+                file.MoveTo(directory + "\\" + SetPrefix(file, prefix));
             }
         }
 
-        private static string GetOptions(List<Photo> list, int i, List<string> options)
+        static string SetPrefix (FileInfo file, string prefix)
         {
-            for (int j = 0; j < options.Count; j++)
-            {
-                if (options[j] == "YY")
-                    return options[j] = options[j].Replace("YY", list[i].Taken.Year.ToString().Substring(2));
-                if (options[j] == "YYYY")
-                    return options[j] = options[j].Replace("YYYY", list[i].Taken.Year.ToString());
-                if (options[j] == "MM")
-                    return options[j] = "0" + options[j].Replace("MM", list[i].Taken.Month.ToString());
-                if (options[j] == "DD")
-                    return options[j] = options[j].Replace("DD", list[i].Taken.Day.ToString());
-                if (options[j] == "/")
-                    return "\\";
-            }
-
-            return null;
+            return file.CreationTimeUtc.ToString(prefix, CultureInfo.InvariantCulture)+ "-" + file.Name;
         }
     }
 }
